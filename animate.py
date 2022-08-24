830cd1288ce8318b19b2ce99f90456fcb4e60434
Miikka Sainio <miikka.sainio@gmail.com>
2022-08-24 15:40:53 +0300

    animation update

--- animate.py
+++ animate.py
@@ -5,9 +5,10 @@ cv2.namedWindow('mahMovie')
 
 # Assumes an ordered sequence of filenames (001.jpg, 002.jpg, etc)
 while True:
-    for filename in sorted(glob.glob('./animate/*.png')):
+    for i in range(50, 10000):
+        filename = 'animate/{}.png'.format(i)
         img = cv2.imread(filename)
         cv2.imshow('mahMovie', img)
-        cv2.waitKey(30)
+        cv2.waitKey(300)
 
 cv2.destroyAllWindows()
