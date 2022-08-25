ae203bdf34e62c3fecebcbf08ac8865601d0e05f
Miikka Sainio <miikka.sainio@gmail.com>
2022-08-25 17:07:00 +0300

    finalize

--- animate.py
+++ animate.py
@@ -1,16 +1,15 @@
 import glob
 import cv2
 
-cv2.namedWindow('mahMovie')
 
-# Assumes an ordered sequence of filenames (001.jpg, 002.jpg, etc)
 while True:
-    for i in range(54, 10000):
+    for i in range(2, 10000):
         try:
             filename = 'animate/{}.png'.format(i)
+            print(filename)
             img = cv2.imread(filename)
-            cv2.imshow('mahMovie', img)
-            cv2.waitKey(33)
+            cv2.imshow('pic', img)
+            cv2.waitKey(100)
         except:
             break
 
